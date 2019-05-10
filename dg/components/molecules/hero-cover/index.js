33b403bb9cdeb9d579bb608c02b52a4746551f8c
Jean-pierre Vincent <jpvirtual@yahoo.com>
2019-05-10 20:45:32 +0200

    cover for desktop

--- dg/components/molecules/hero-cover/index.js
+++ dg/components/molecules/hero-cover/index.js
@@ -8,7 +8,7 @@ customElements.define('axa-hero-cover', class HeroCover extends HTMLElement {
         //const dumbDiv = document.createElement('div')
         this.classList.add('m-hero-cover')
 
-        const imgURL = this.getAttribute('data-src') || 'https://image-placeholder.com/images/actual-size/1600x1200.png'
+        const imgURL = this.getAttribute('+src') || 'https://imgplaceholder.com/160x90?text=16++9&font-size=50'
 
         this.style.backgroundImage = `url(${imgURL})`
 
@@ -17,27 +17,33 @@ customElements.define('axa-hero-cover', class HeroCover extends HTMLElement {
             cta = new AxaButton()
             cta.setAttribute('is', 'axa-button')
             cta.textContent = 'Read More'
-            cta.setAttribute('data-icon', 'arrow-right')
+            // cta.setAttribute('+icon', 'arrow-right')
         }
 
         let content = this.querySelector('[slot=text-content]')
         if (!content) content = '&nbsp;'
         else content = content.innerHTML
 
+        let legalContent = this.querySelector('[slot=legal-content]')
+
+
+
         // only 2 options allowed
-        let direction = this.getAttribute('data-direction') === 'rtl' ? 'rtl' : 'ltr'
-        let design = this.getAttribute('data-design') === 'wob' ? 'wob' : 'bow'
+        let direction = this.getAttribute('+direction') === 'rtl' ? 'rtl' : 'ltr'
+        let design = this.getAttribute('+design') === 'wob' ? 'wob' : 'bow'
 
         this.innerHTML = `
             <div class="m-hero-cover__text-background m-hero-cover__text-background--${direction} m-hero-cover__text-background--${direction}--${design}">
-                <h5 class=a-typo__title>
-                ${this.getAttribute('data-category-title') || 'Category title'}</h5>
-                <h2 class=a-typo__page-title>${this.getAttribute('data-main-title') || 'Main Title'}</h2>
-                <img style=display:none alt= src=${imgURL} />
-                <p class="a-typo__text">${content}</p>
-                <slot name=call-to-action />
+                <div class=m-hero-cover__content>
+                    <h5 class=a-typo__tagline>${this.getAttribute('+category-title') || 'Category title'}</h5>
+                    <h2 class=a-typo__page-title>${this.getAttribute('+main-title') || 'Main Title'}</h2>
+                    <p class="a-typo__text">${content}</p>
+                    ${legalContent ? `<p class="a-typo__legals">${legalContent.innerHTML}</p>`:''}
+                    <slot name=call-to-action />
+                </div>
             </div>
-        `
+            <img style=display:none alt= src=${imgURL} />
+            `
 
         this.querySelector('slot[name=call-to-action]').appendChild(cta)
     }
