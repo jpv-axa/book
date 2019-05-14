119e09cef0b24de5c9cbab1c6ca457daf2785268
Jean-pierre Vincent <jpvirtual@yahoo.com>
2019-05-14 17:49:45 +0200

    more demo for typo

--- dg/stories/atoms.js
+++ dg/stories/atoms.js
@@ -45,7 +45,7 @@ storiesOf('Typography')
         <h2 class="a-typo__slice-title" title=".a-typo__slice-title">Slice Title - H2</h2>
         <h3 class="a-typo__module-title" title=".a-typo__module-title">Module Title - H3</h3>
         <h4 class="a-typo__card-title" title=".a-typo__card-title">Card Title - H4</h4>
-        <h4 class="a-typo__text-title" title=".a-typo__text-title">Text Title - H5</h4>
+        <h5 class="a-typo__text-title" title=".a-typo__text-title">Text Title - H5</h5>
     `)
     .add('Texts', () => `
         <p><quote class="a-typo__highlight" title=".a-typo__highlight | .a-typo__quote">Highlight / Quote</quote></p>
@@ -61,7 +61,16 @@ storiesOf('Typography')
         <p><span class="a-typo__legals" title=".a-typo__legals">Legals</span></p>
     `)
     .add('Long text demos', () => `
-        <h1 class=a-typo__page-title>Unser Tipp für Ihre Mopedversicherung</h1>
-        <p class=a-typo__text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-
+        <p class="a-typo__legals a-typo__relative-spacing-top__2" title=.a-typo__legals>Terms and conditions apply. Accident Forgiveness is a step back, so you won’t lose your full No Claims Discount if you have an accident. For customers with one or more years of No Claims Discount. Max 2 claims with full NCD.</p>
+        <p class="a-typo__primary-link a-typo__relative-spacing-top__2" title=a-typo__primary-link>BUTTON / PRIMARY LINK / TAGLINE / MENU ITEM</p>
+        <p class="a-typo__secondary-text a-typo__relative-spacing-top__2" title=.a_typo__secondary-text>Entdecken Sie die große Auswahl an attraktiven Preisvorteilen und Informationen rund um die Themen Gesundheit, Familie, Reisen, Auto, Datensicherheit und Recht. Darüber hinaus bieten wir Ihnen spezielle Informationen für Geschäftskunden und den öffentlichen Dienst. </p>
+        <p class="a-typo__text a-typo__relative-spacing-top__2" title=.a-typo__text>Sie genießen bereits den Schutz, auf den sich jeden Tag rund um die Uhr Millionen von Menschen in Deutschland verlassen können? Dann empfehlen Sie AXA im Freundeskreis ganz einfach per E-Mail, WhatsApp, Facebook oder Twitter. Damit sich auch Ihre Freunde schon bald auf den ausgezeichneten Versicherungsschutz von AXA verlassen können.</p>
+        <p> <label class="a-typo__label a-typo__relative-spacing-top__2" title=.a-typo__label>Alternative phone numbers</label><br>
+        <p> <quote class="a-typo__highlight a-typo__relative-spacing-top__2" title=.a-typo__highlight>Enjoy easy access to AXA's 24/7 emergency and claims helpline plus Accident Forgiveness as a part of your policy. With Accident Forgiveness you won’t lose your No Claims Discount just for having one accident.</quote>
+        <h5 class="a-typo__text-title a-typo__relative-spacing-top__2" title=.a-typo__text-title>Important Information: Please read before purchasing travel insurance</h5>
+        <h4 class="a-typo__card-title a-typo__relative-spacing-top__2" title=.a-typo__card-title>Änderungen an einem bestehenden Vertrag</h4>
+        <h3 class="a-typo__module-title a-typo__relative-spacing-top__2" title=.a-typo__module-title>Compare our features and benefits and buy the right car insurance policy for you.</h3>
+        <h2 class="a-typo__slice-title a-typo__relative-spacing-top__2" title=.a-typo__slice-title>Die Private Haftpflichtversicherung: Schutz schon ab 21,13€ im Jahr*</h2>
+        <h1 class="a-typo__page-title a-typo__relative-spacing-top__2" title=.a-typo__page-title>Die Private Haftpflichtversicherung: Schutz schon ab 21,13€ im Jahr*</h1>
+        <h1 class="a-typo__event-title a-typo__relative-spacing-top__2" title=.a-typo__event-title>Know You Can</h1>
 `)
