5230d911ed249fc54ac6f5b954dc357b27866413
Jean-pierre Vincent <jpvirtual@yahoo.com>
2019-05-14 16:26:56 +0200

    redo gradients, change typo__text to typo__highlight, update spacings

--- dg/components/atoms/button/index.js
+++ dg/components/atoms/button/index.js
@@ -22,7 +22,7 @@ class AxaButton extends HTMLButtonElement {
 
         this.textContent = this.textContent || '! AxaButton !'
         this.classList.add('a-button')
-        //this.classList.add('a-button--motion')
+        this.classList.add('a-button--motion')
         this.classList.add('a-typo__button')
 
     }
