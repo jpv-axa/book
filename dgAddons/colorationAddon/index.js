eec476682d0bda4e760861291893c5350efa448e
Jean-pierre Vincent <jpvirtual@yahoo.com>
2019-06-27 15:01:49 +0200

    remove console.log

--- dgAddons/colorationAddon/index.js
+++ dgAddons/colorationAddon/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import addons from '@storybook/addons';
 
-const withCode = function(code, type , storyFn = null) {
-  const emitAddCode = ({ kind, story }) => {
-    console.log("emitAddCode " + type + "/code/" + code + "/story/" + story);
-    addons.getChannel().emit(`soft/code/add_${type}`, { code, type });
+const withCode = function (code, type, storyFn = null) {
+  const emitAddCode = ({
+    kind,
+    story
+  }) => {
+    //console.log("emitAddCode " + type + "/code/" + code + "/story/" + story);
+    addons.getChannel().emit(`soft/code/add_${type}`, {
+      code,
+      type
+    });
   };
-  return (storyFn, { kind, story }) => {
-      emitAddCode({ kind, story });
-      return storyFn();
-    };
+  return (storyFn, {
+    kind,
+    story
+  }) => {
+    emitAddCode({
+      kind,
+      story
+    });
+    return storyFn();
+  };
 };
 
 export default withCode;
