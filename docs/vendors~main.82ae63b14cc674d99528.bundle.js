cf32f0bfde79e5fe20d2c6794568b938c2534646
Jean-pierre Vincent <jpvirtual@yahoo.com>
2019-06-27 18:54:25 +0200

    new demo

--- docs/vendors~main.82ae63b14cc674d99528.bundle.js
+++ docs/vendors~main.82ae63b14cc674d99528.bundle.js
@@ -71,4 +71,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",function(){return isPlainObject})}]]);
-//# sourceMappingURL=vendors~main.f492f71232f929c8d8e8.bundle.js.map
+//# sourceMappingURL=vendors~main.82ae63b14cc674d99528.bundle.js.map
