a5cb4e5636750788bc2eaa43c06e31a78b98a19e
Jean-pierre Vincent <jpvirtual@yahoo.com>
2019-07-02 10:18:17 +0200

    move the images from AXA UK servers to local

--- dg/stories/molecules-hero-cover.js
+++ dg/stories/molecules-hero-cover.js
@@ -3,6 +3,8 @@ import {
     addDecorator
 } from '@storybook/html'
 
+import imageWhite from './statics/home-hero-8.jpg'
+import imageDark from './statics/landlord-hero-3.jpg'
 
 import withCode from '../../dgAddons/colorationAddon';
 
@@ -23,7 +25,7 @@ const demo2 = `
 <axa-hero-cover 
     +category-title="+category-title value" 
     +main-title="+main-title value"
-    +src="https://www.axa.co.uk/globalassets/new-website/home/home-hero-8.jpg""
+    +src="${imageWhite}"
     >
     <p slot=text-content>Place any content here with the attribute <code>slot=text-content</code>. Even <a href="/">links</a> or your custom styles are accepted, as long as they comply with the general guidelines</p>
     <p slot=legal-content>Optional <code>slot=legal-content</code> content.</p>        
@@ -33,7 +35,7 @@ const demo2 = `
 <axa-hero-cover 
     +category-title="motorcycle insurance" 
     +main-title="Die Private Haftpflichtversicherung: Schutz schon ab 21,13€ im Jahr*"
-    +src="https://www.axa.co.uk/globalassets/new-website/home/home-hero-8.jpg""
+    +src="${imageWhite}""
     >
     <p slot=text-content>Enjoy easy access to AXA's 24/7 emergency and claims helpline plus Accident Forgiveness as a part of your policy. With Accident Forgiveness you won’t lose your No Claims Discount just for having one accident.</p>
     <p slot=legal-content>Terms and conditions apply. Accident Forgiveness is a step back, so you won’t lose your full No Claims Discount if you have an accident. For customers with one or more years of No Claims Discount. Max 2 claims with full NCD.</p>        
@@ -48,7 +50,7 @@ const demo3 = `
     <axa-hero-cover 
         +main-title="Invert direction : rtl"
         +direction=rtl
-        +src="https://www.axa.co.uk/globalassets/new-website/landlord/landlord-hero-3.jpg">
+        +src="${imageDark}">
         <p slot=text-content>The value of the attribute <code>+direction</code> can be either <code>rtl</code> (Right To Left, like here) or <code>ltr</code> (default).<br>
         The value of the attribute <code>+design</code> can be either <code>wob</code> (White On Black, like here) or <code>bow</code> (default).</p>
     </axa-hero-cover>
@@ -56,7 +58,7 @@ const demo3 = `
     <axa-hero-cover 
         +main-title="Invert design : wob"
         +design=wob
-        +src="https://www.axa.co.uk/globalassets/new-website/home/home-hero-8.jpg"">
+        +src="${imageWhite}"">
         <p slot=text-content>The value of the attribute <code>+direction</code> can be either <code>rtl</code> (Right To Left) or <code>ltr</code> (default, like here).<br>
         The value of the attribute <code>+design</code> can be either <code>wob</code> (White On Black, like here) or <code>bow</code> (default).</p>
     </axa-hero-cover>
@@ -65,14 +67,14 @@ const demo3 = `
         +main-title="Invert both design (wob) and direction (rtl)"
         +direction=rtl
         +design=wob
-        +src="https://www.axa.co.uk/globalassets/new-website/landlord/landlord-hero-3.jpg" >
+        +src="${imageDark}" >
         <p slot=text-content>The value of the attribute <code>+direction</code> can be either <code>rtl</code> (Right To Left, like here) or <code>ltr</code> (default).<br>
         The value of the attribute <code>+design</code> can be either <code>wob</code> (White On Black, like here) or <code>bow</code> (default).</p>
     </axa-hero-cover>
 
     <axa-hero-cover 
         +main-title="All default values (ltr / bow)"
-        +src="https://www.axa.co.uk/globalassets/new-website/home/home-hero-8.jpg" >
+        +src="${imageWhite}" >
         <p slot=text-content>The values <code>+design</code> and <code>+direction</code> are left to default.</p>
     </axa-hero-cover>
 `
@@ -84,7 +86,7 @@ const demo4 = `
 <axa-hero-cover 
     +direction=rtl
     +design=wob
-    +src="https://www.axa.co.uk/globalassets/new-website/landlord/landlord-hero-3.jpg" >
+    +src="${imageDark}" >
     <p slot=text-content>You can add from 1 to 2 buttons* in a <code>slot=call-to-action</code>.</p>
     <p slot=legal-content>*Technically you can add anything but as a design system we dont recommend nor support anything else.</p>
     <div slot=call-to-action>
@@ -94,7 +96,7 @@ const demo4 = `
 </axa-hero-cover>
 
 <axa-hero-cover
-    +src="https://www.axa.co.uk/globalassets/new-website/home/home-hero-8.jpg" >
+    +src="${imageWhite}" >
     <p slot=text-content>You can add from 1 to 2 buttons in a <code>slot=call-to-action</code>.</p>
     <div slot=call-to-action>
         <axa-button>資料請求</axa-button>
