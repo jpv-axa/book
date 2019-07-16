f5ab8957255cf9fcc8d13f3ad5b630592a24223b
Jean-pierre Vincent <jpvirtual@yahoo.com>
2019-07-16 12:07:21 +0200

    new demo for fields now respect guidelines for spacings

--- dg/stories/atom-input-text.js
+++ dg/stories/atom-input-text.js
@@ -12,12 +12,21 @@ const infoShort = 'Text helper message.'
 
 const styles = `
 <style>
+@media (min-width: 37.5rem) {
 axa-input {
 	margin-right: 2rem;
+}
 }
 fieldset {
 	margin-bottom: 2rem;
 	padding-top: 2rem;
+	background-color:antiquewhite;
+	border:0;
+	padding:0 1rem;
+	margin:0;
+}
+legend {
+	padding-bottom: 2rem;
 }
 </style>
 `
