5519bc245762ccfe0c3aea482532d954cd40def8
Jean-pierre Vincent <jpvirtual@yahoo.com>
2019-07-23 14:56:20 +0200

    init() method is generic enough to go to the mother class

--- dg/components/atoms/input/input-text.js
+++ dg/components/atoms/input/input-text.js
@@ -5,27 +5,7 @@ class textInput extends commonInput {
 		this.el = el
 	}*/
 	init() {
-		this.getOrSetId()
-		let label = this.setupLabel()
-		let errorMessage = this.getError()
-		if (errorMessage)
-			this.el.field.setAttribute('aria-describedby', this.generateErrorFieldId())
-
-		this.el.insertAdjacentElement('afterbegin', label)
-		let field = this.el.field
-		let info = this.el.querySelector('[slot=info]')
-		//debugger
-		// Accessibility is setup, let's rebuild the HTML
-		this.empty()
-		this.el.appendChild(label)
-		this.el.appendChild(field)
-		if (errorMessage)
-			this.el.appendChild(errorMessage)
-		if (info)
-			this.el.appendChild(info)
-
-
-		this.disabled = this.el.field.disabled
+		super.init()
 	}
 }
 
