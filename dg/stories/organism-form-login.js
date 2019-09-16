f3b2a052c57f43495d34f365c1f8631e82e1252d
Jean-pierre Vincent <jpvirtual@yahoo.com>
2019-09-16 17:02:05 +0200

    use the new form container in the demos

--- dg/stories/organism-form-login.js
+++ dg/stories/organism-form-login.js
@@ -1,12 +1,10 @@
-import {
-	storiesOf,
-	addDecorator
-} from '@storybook/html'
-import withCode from '../../dgAddons/colorationAddon';
+import { storiesOf, addDecorator } from '@storybook/html'
+import withCode from '../../dgAddons/colorationAddon'
 
 import '../components/atoms/input'
 
 const demo1 = `
+<axa-form>
 <form onsubmit="alert('submited');return false;">
 
 <axa-input +label="Your Login">
@@ -18,27 +16,12 @@ const demo1 = `
 	<p slot=info>A good password contains at least <strong>8 characters</strong>, including at least <strong>two numbers</strong> and two <strong>punctuation marks</strong>. Spaces matter, so why not write an easy-to-remember sentence?</p>
 </axa-input>
 
-<p>
+<div class=m-form__cta>
 	<axa-button>Submit</axa-button>
-</p>
+</div>
 
 </form>
-
-<style>
-@media (min-width: 37.5rem) {
-axa-input {
-	margin-right: 2rem;
-}
-}
-form {
-	padding: 1rem;
-}
-p {
-	padding:0;
-	margin:0;
-}
-</style>
-
+<axa-form>
 `
 
 storiesOf('Organism|Form', module)
